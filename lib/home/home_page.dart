f18bd1cbabdc5100d255a9c390de281f6810736f
Ladan_Rajabian <ladanrajabian@gmail.com>
2023-11-03 16:31:11 +0100

    finished card-homepage

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -19,10 +19,10 @@ class HomeWidget extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
-            const Gap(50),
+            const Gap(40),
             SizedBox(
                 height: 200,
-                width: 390,
+                width: 370,
                 child: Stack(
                   alignment: Alignment.center,
                   children: [
@@ -48,65 +48,109 @@ class HomeWidget extends StatelessWidget {
                           width: 100,
                           decoration: BoxDecoration(
                               image: const DecorationImage(
-                                  image: AssetImage(
-                                      'assets/image/recipescard.png')),
+                                  image:
+                                      AssetImage('assets/image/recipes.png')),
                               borderRadius: BorderRadius.circular(500),
                               color: Colors.black)),
                     ),
                     Container(
-                      margin: const EdgeInsets.only(left: 100),
+                      margin: const EdgeInsets.only(left: 90),
                       child: const Text(
                         'Recipes',
                         style: TextStyle(
-                            fontSize: 28, fontWeight: FontWeight.w600),
+                            fontSize: 24, fontWeight: FontWeight.w600),
                       ),
                     )
                   ],
                 )),
-            const SizedBox(
-              height: 40,
-            ),
-            Card(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular((30))),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  gradient: const LinearGradient(
-                    colors: [
-                      Color.fromRGBO(0, 158, 255, 0.75),
-                      Color.fromRGBO(0, 231, 255, 0.5)
-                    ],
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                  ),
-                ),
-                height: 153,
-                width: 350,
-              ),
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            Card(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular((30))),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  gradient: const LinearGradient(
-                    colors: [
-                      Color.fromRGBO(120, 149, 203, 1),
-                      Color.fromRGBO(74, 85, 162, 1)
-                    ],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomRight,
-                  ),
-                ),
-                height: 153,
-                width: 350,
-              ),
-            ),
+            const Gap(10),
+            SizedBox(
+                height: 200,
+                width: 370,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        gradient: const LinearGradient(
+                          colors: [
+                            Color.fromRGBO(0, 158, 255, 0.75),
+                            Color.fromRGBO(0, 231, 255, 0.5)
+                          ],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                        ),
+                      ),
+                      height: 153,
+                      width: 350,
+                    ),
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: Container(
+                          height: 100,
+                          width: 100,
+                          decoration: BoxDecoration(
+                              image: const DecorationImage(
+                                  image: AssetImage(
+                                'assets/image/waterReminder.png',
+                              )),
+                              borderRadius: BorderRadius.circular(600),
+                              color: Colors.black)),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(right: 50),
+                      child: const Text(
+                        'Reminder to drink water',
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.w500),
+                      ),
+                    )
+                  ],
+                )),
+            const Gap(10),
+            SizedBox(
+                height: 200,
+                width: 370,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        gradient: const LinearGradient(
+                          colors: [
+                            Color.fromRGBO(120, 149, 203, 1),
+                            Color.fromRGBO(74, 85, 162, 1)
+                          ],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomRight,
+                        ),
+                      ),
+                      height: 153,
+                      width: 350,
+                    ),
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Container(
+                          height: 100,
+                          width: 100,
+                          decoration: BoxDecoration(
+                              image: const DecorationImage(
+                                  image: AssetImage('assets/image/sport.png')),
+                              borderRadius: BorderRadius.circular(500),
+                              color: Colors.black)),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(left: 90),
+                      child: const Text(
+                        'Sport exercises',
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.w600),
+                      ),
+                    )
+                  ],
+                )),
           ],
         ),
       ),
